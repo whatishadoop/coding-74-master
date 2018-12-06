2be73582d1564bea4292487881c42d02ebeacf3b
jin <jinlu.hansome@163.com>
2018-12-06 17:16:52 +0800

    测试idea

--- src/components/modules/forumpage.vue
+++ src/components/modules/forumpage.vue
@@ -1,12 +1,29 @@
 <template>
-    <div>
-      论坛页面
+  <div>
+    <div style="margin: 20px;">
+      <h1>{{$t("message.title")}}</h1>
+      <input style="width: 300px;" class="form-control" :placeholder="$t('placeholder.enter')">
+      <ul>
+        <li v-for="brand in brands">{{brand}}</li>
+      </ul>
     </div>
+    <button type="button" class="btn btn-success" @click="changeLocale">中文/EN</button>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
     export default {
-
+      data () {
+        return {
+          brands: [this.$t('brands.nike'), this.$t('brands.adi'), this.$t('brands.nb'), this.$t('brands.ln')]
+        };
+      },
+      methods: {
+        changeLocale() {
+          let locale = this.$i18n.locale;
+          locale === 'zh' ? this.$i18n.locale = 'en' : this.$i18n.locale = 'zh';
+        }
+      }
     };
 </script>
 
--- src/components/modules/homepage.vue
+++ src/components/modules/homepage.vue
@@ -52,12 +52,9 @@
       </div>
     </div>
   </div>
-  </div>
 </template>
 
 <script type="text/ecmascript-6">
-
-
   export default {
     data() {
       return {
